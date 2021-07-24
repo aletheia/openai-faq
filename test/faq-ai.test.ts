b83fffb8450d6996c22e7554f698219ed6939c77
aletheia <bianchi.luca@gmail.com>
2021-07-24 16:06:59 +0200

    :sparkles: Add tests and readme

---
+++ test/faq-ai.test.ts
@@ -0,0 +1,19 @@
+// import {expect as expectCDK, matchTemplate, MatchStyle} from '@aws-cdk/assert';
+// import * as cdk from '@aws-cdk/core';
+// import * as FaqAi from '../lib/faq-ai-stack';
+
+test('Empty Stack', () => {
+  // const app = new cdk.App();
+  // WHEN
+  // const stack = new FaqAi.FaqAiStack(app, 'MyTestStack');
+  // THEN
+  // expectCDK(stack).to(
+  //   matchTemplate(
+  //     {
+  //       Resources: {},
+  //     },
+  //     MatchStyle.EXACT
+  //   )
+  // );
+  expect(true).toBeTruthy();
+});
